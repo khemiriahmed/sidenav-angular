ddd8caa0c1d589ede65c48dbe721fa3cb98e2847
khemiriahmed <ahmed.12virageca@gmail.com>
2022-04-25 23:28:38 +0200

    First commit

---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,38 @@
+export const navbarData=[
+{
+    routLink:'dashbord',
+    icon:'fal fa-home',
+    label:'Dashbord'
+},
+{
+    routLink:'products',
+    icon:'fal fa-box-open',
+    label:'Products'
+},
+{
+    routLink:'statistics',
+    icon:'fal fa-chart-bar',
+    label:'Statistics'
+},
+{
+    routLink:'coupens',
+    icon:'fal fa-tags',
+    label:'coupens'
+},
+{
+    routLink:'pages',
+    icon:'fal fa-file',
+    label:'Pages'
+},
+{
+    routLink:'media',
+    icon:'fal fa-camera',
+    label:'Media'
+},
+{
+    routLink:'settings',
+    icon:'fal fa-cog',
+    label:'Settings'
+},
+
+];
